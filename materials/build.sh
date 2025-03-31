badf4d3f93618b413a37fb2883e79b6d37811271
Matteo Francia <m.francia@unibo.it>
2025-03-31 16:48:52 +0200

    add slides

--- materials/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -exo
-for FILE in *.ipynb; do 
-    echo "Processing $FILE file...";
-    filename=$(basename -- "$FILE")
-    extension="${filename##*.}"
-    filename="${filename%.*}"
-    jupyter nbconvert --execute --to notebook --inplace "$FILE"
-done
